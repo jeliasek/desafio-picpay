04b09b2815e8711ad5f3d53f6ee54ea635a7b8da
Joao <jeliasek@gmail.com>
2023-08-28 20:54:16 -0300

    Feat (Desafio-Picpay): Create Transaction Entity

---
+++ src/main/java/com/desafiopicpay/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.desafiopicpay.domain.transaction;
+
+import com.desafiopicpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity(name = "transaction")
+@Table(name = "transaction")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "value")
+    private BigDecimal value;
+
+    @ManyToOne
+    @JoinColumn(name = "senderId")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiverId")
+    private User receiver;
+
+    @Column(name = "date")
+    private LocalDate date;
+}
